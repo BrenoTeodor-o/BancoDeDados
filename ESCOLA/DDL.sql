0300a881d2f86a5ac42471a3e5c6a5122307ff9a
BrenoTeodor-o <teodoro.breno@outlook.com.br>
2018-10-27 14:10:00 -0300

    Now adding the view file to start the homework

--- ESCOLA/DDL.sql
+++ ESCOLA/DDL.sql
@@ -1,50 +1,50 @@
 ﻿
--- Table: escola."Disciplina"
+-- -- Table: escola."Disciplina"
 
--- DROP TABLE escola."Disciplina";
+-- -- DROP TABLE escola."Disciplina";
 
-CREATE TABLE escola."Disciplina"
-(
-  cod serial NOT NULL,
-  nome character varying,
-  cargaHoraria integer,
-  ano integer(4),
-  CONSTRAINT pk_disciplina PRIMARY KEY (cod)
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE escola."Disciplina"
-  OWNER TO postgres;
+-- CREATE TABLE escola."Disciplina"
+-- (
+--   cod serial NOT NULL,
+--   nome character varying,
+--   cargaHoraria integer,
+--   ano integer(4),
+--   CONSTRAINT pk_disciplina PRIMARY KEY (cod)
+-- )
+-- WITH (
+--   OIDS=FALSE
+-- );
+-- ALTER TABLE escola."Disciplina"
+--   OWNER TO postgres;
 
--- Table: escola."Professor"
+-- -- Table: escola."Professor"
 
--- DROP TABLE escola."Professor";
+-- -- DROP TABLE escola."Professor";
 
-CREATE TABLE escola."Professor"
-(
-  cod serial NOT NULL,
-  nome character varying,
-  CONSTRAINT pk_professor PRIMARY KEY (cod)
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE escola."Professor"
-  OWNER TO postgres;
+-- CREATE TABLE escola."Professor"
+-- (
+--   cod serial NOT NULL,
+--   nome character varying,
+--   CONSTRAINT pk_professor PRIMARY KEY (cod)
+-- )
+-- WITH (
+--   OIDS=FALSE
+-- );
+-- ALTER TABLE escola."Professor"
+--   OWNER TO postgres;
 
--- Table: escola."Curso"
+-- -- Table: escola."Curso"
 
--- DROP TABLE escola."Curso";
+-- -- DROP TABLE escola."Curso";
 
-CREATE TABLE escola."Curso"
-(
-  cod serial NOT NULL,
-  nome character varying,
-  CONSTRAINT pk_curso PRIMARY KEY (cod)
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE escola."Curso"
-  OWNER TO postgres;
+-- CREATE TABLE escola."Curso"
+-- (
+--   cod serial NOT NULL,
+--   nome character varying,
+--   CONSTRAINT pk_curso PRIMARY KEY (cod)
+-- )
+-- WITH (
+--   OIDS=FALSE
+-- );
+-- ALTER TABLE escola."Curso"
+--   OWNER TO postgres;
---
+++ ESCOLA/escola3.sql
@@ -0,0 +1,18 @@
+-- 5) Baseado nos exercícios anteriores, reconstrua as consultas SQL usando visões:
+
+-- 5.1 Visão que retorne todos os alunos, disciplina e curso
+--    5.1.1 usando a visão anterior uma consulta que retorne todos os alunos de um determinado curso, selecionado pelo código, ordenado pelo nome do aluno
+--    5.1.2 - Idem ao 5.1.1, porém selecionado pelo nome do curso
+
+-- 5.2 - usando a visão anterior, todas as notas dos alunos
+--    5.2.1 usando esta visão, todas as notas de um determinado aluno, selecionadas pelo RA do aluno, ordenadas pelo bimestre
+
+-- 5.3 - Uma visão com todos os professores de um determinado curso
+--    5.3.1 - usando esta visão, todas as disciplinas de um determinado professor, selecionado pela matrícula do professor
+--    5.3.2 a média de salários por curso
+
+
+-- 5.4 a média de nota de turma de uma determinada disciplina
+
+
+-- 5.5 os cursos onde o valor total de salários é maior que 100.000
